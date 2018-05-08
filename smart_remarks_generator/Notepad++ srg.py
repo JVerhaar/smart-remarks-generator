d620e038df35e1c1405259b8e427fa9eb2a1a19b
Jessica Verhaar <0907856@hr.nl>
2018-05-08 10:08:05 +0200

    everything

---
+++ smart_remarks_generator/Notepad++ srg.py
@@ -0,0 +1,5 @@
+import random
+smrmlist = ["I don't have the time or crayons to explain myself to you", "Don't be ashamed of who you are. That's your parent's job", "Sarcasm: Because murder charges are expensive."]
+print (random.choice(smrmlist))
+
+PS C:\Users\jessi\OneDrive\smart_remarks_generator\smart_remarks_generator> .\smart_remarks_generator.py
---
+++ smart_remarks_generator/smart_remarks_generator/smart_remarks_generator.py
@@ -0,0 +1,3 @@
+import random
+smrmlist = ["I don't have the time or crayons to explain myself to you", "Don't be ashamed of who you are. That's your parent's job", "Sarcasm: Because murder charges are expensive."]
+print (random.choice(smrmlist))
